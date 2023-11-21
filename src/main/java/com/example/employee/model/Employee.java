187bedebf03b620a840852a69645bdb2919f718f
Ramirez <ramirez.gfilho@gmail.com>
2023-11-21 12:53:48 +0100

    second commit

--- src/main/java/com/example/employee/model/Employee.java
+++ src/main/java/com/example/employee/model/Employee.java
@@ -6,6 +6,7 @@ import jakarta.persistence.*;
 @Table(name="employee")
 public class Employee {
 
+    //Comment one
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "emp_id")
